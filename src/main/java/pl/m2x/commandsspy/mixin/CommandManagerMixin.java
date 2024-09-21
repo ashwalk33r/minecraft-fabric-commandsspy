ddac59bc2c0bc0f67866b47a7e0fbe7660c7bff9
Ash <ashwalk33r@gmail.com>
2024-09-21 11:58:41 +0200

    feat: add config - blacklist, logArguments

--- src/main/java/pl/m2x/commandsspy/mixin/CommandManagerMixin.java
+++ src/main/java/pl/m2x/commandsspy/mixin/CommandManagerMixin.java
@@ -10,21 +10,29 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import pl.m2x.commandsspy.CommandsSpy;
+import pl.m2x.commandsspy.CommandsSpyCommand;
 
 @Mixin(CommandManager.class)
 public class CommandManagerMixin {
     @SuppressWarnings({ "PMD.UnusedPrivateMethod", "PMD.UnusedFormalParameter" })
     @Inject(method = "execute", at = @At("HEAD"))
-    private void onCommandExecute(ParseResults<ServerCommandSource> parseResults, String command, CallbackInfo ci) {
+    private void onCommandExecute(ParseResults<ServerCommandSource> parseResults, String fullCommand, CallbackInfo ci) {
+        String command = CommandsSpyCommand.getCommand(fullCommand);
+        if (CommandsSpy.BLACKLIST.isBlacklisted(command)) {
+            return;
+        }
+
         ServerCommandSource source = parseResults.getContext().getSource();
         Entity entity = source.getEntity();
 
+        String commandToLog = CommandsSpy.CONFIG.logArguments ? fullCommand : command;
+
         if (entity instanceof ServerPlayerEntity player) {
             String playerName = player.getName().getString();
-            CommandsSpy.logCommand(command, "Player: " + playerName);
+            CommandsSpy.logCommand(commandToLog, "Player: " + playerName);
         } else {
-            // Other sources (e.g., functions, data packs, signs, command blocks)
-            CommandsSpy.logCommand(command, source.getName());
+            // Other sources (e.g., functions, data packs, signs, fullCommand blocks)
+            CommandsSpy.logCommand(commandToLog, source.getName());
         }
     }
 }
